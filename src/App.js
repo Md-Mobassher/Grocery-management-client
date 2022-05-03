8b72c4583b3c025a30b607456760c81bf088fa95
Md-Mobassher <you@example.com>
2022-05-03 21:17:32 +0600

    header added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './Pages/Shared/Header/Header';
 import Home from './Pages/Home/Home/Home';
 import Login from './Pages/Login/Login/Login';
 import SignUp from './Pages/Login/SignUp/SignUp';
 import Footer from './Pages/Shared/Footer/Footer';
-import Header from './Pages/Shared/Header/Header';
+
 
 import NotFound from './Pages/Shared/NotFound/NotFound';
 
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,10 +1,48 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import {useAuthState} from "react-firebase-hooks/auth"
+import auth from '../../../firebase.init';
+import logo from '../../../Images/logo/logo.png'
 
 const Header = () => {
+    const [user] = useAuthState(auth)
+
+    const handleSignOut = () =>{
+        signOut(auth);
+    }
+
     return (
-        <div>
-            <h1>Header</h1>
-        </div>
+      <>
+        <Navbar collapseOnSelect expand="lg" sticky='top' bg='light' variant="light">
+                <Container className='header-text'>
+                    <Navbar.Brand as={Link} to="/">
+                            <img src={logo} alt="" />
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav" >
+                        <Nav className="me-auto">
+                            <Nav.Link href="home#services">Services</Nav.Link>
+                            <Nav.Link href="home#gallery">Gallery</Nav.Link>
+                        </Nav>
+                        <Nav>
+                            <Nav.Link as={Link} to="checkout">Checkout</Nav.Link>
+                            <Nav.Link as={Link} to="about">About</Nav.Link>
+                            <Nav.Link as={Link} to="blog">Blog</Nav.Link>
+                            {
+                                user ?
+                                    <button className='btn btn-link text-white text-decoration-none' onClick={handleSignOut}>Sign Out</button>
+                                :
+                                <Nav.Link as={Link} to="login">
+                                Login
+                            </Nav.Link>}
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+      
+      </>
     );
 };
 

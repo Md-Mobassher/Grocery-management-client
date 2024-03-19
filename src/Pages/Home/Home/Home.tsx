3f02447ca0441d581046f8098f46de6651a40ca9
Md Mobassher Hossain <mobassherpautex@gmail.com>
2024-03-19 17:44:43 +0600

    redux and layout component added

---
+++ src/Pages/Home/Home/Home.tsx
@@ -0,0 +1,12 @@
+const Home = () => {
+  return (
+    <div>
+      {/* <Banner></Banner> */}
+      {/* <Inventories></Inventories>
+      <Offer></Offer>
+      <Client></Client> */}
+    </div>
+  );
+};
+
+export default Home;

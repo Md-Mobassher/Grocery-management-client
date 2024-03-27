96f2cf4848b844fe38b993d3e9de2f0874603bf8
Md Mobassher Hossain <mobassherpautex@gmail.com>
2024-03-27 15:50:48 +0600

    baseApi updated

--- src/components/common/Offer.tsx
+++ src/components/common/Offer.tsx
@@ -12,20 +12,24 @@ const Offer = () => {
         backgroundSize: "cover",
       }}
     >
-      <div className=" mt-5 flex justify-center items-center p-5">
-        <div className="">
-          <h2 className="text-4xl font-bold italic">
-            Special Discount For All
+      <div className="max-w-7xl mx-auto flex lg:justify-start md:justify-start items-center my-16 p-5">
+        <div className="max-w-2xl">
+          <h2 className="text-3xl font-bold overflow-hidden italic">
+            Special Discount For <br /> All Grocery Products
           </h2>
-          <h2 className="text-4xl font-bold italic">Grocery Products</h2>
-          <p className="mt-4 ">
+
+          <p className="mt-5 ">
             There is a big opportunity for all of our customers. If you buy any
             grocery items in our shop, you will get maximum 40% discount because
-            of the Eid-Ul-Fitor.{" "}
+            of the Eid-Ul-Fitor.
           </p>
-          <p>Happy Shopping</p>
+          <p className="text-xl font-semibold mt-3">Happy Shopping</p>
 
-          <Button type="submit">Shop Now</Button>
+          <div className="mt-5">
+            <Button className="bg-green-400 hover:bg-green-500 " type="submit">
+              Shop Now
+            </Button>
+          </div>
         </div>
 
         <div className=""></div>
--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,5 +1,15 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import {
+  BaseQueryApi,
+  BaseQueryFn,
+  createApi,
+  DefinitionType,
+  FetchArgs,
+  fetchBaseQuery,
+} from "@reduxjs/toolkit/query/react";
 import { RootState } from "../store";
+import { toast } from "sonner";
+import { logout, setUser } from "../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
   // baseUrl: "https://grocery-management-server.vercel.app/api/v1",
@@ -11,11 +21,61 @@ const baseQuery = fetchBaseQuery({
     if (token) {
       headers.set("authorization", `$(token)`);
     }
+
+    return headers;
   },
 });
 
+const baseQueryWithRefreshToken: BaseQueryFn<
+  FetchArgs,
+  BaseQueryApi,
+  DefinitionType
+> = async (args, api, extraOptions): Promise<any> => {
+  let result = await baseQuery(args, api, extraOptions);
+
+  if (result?.error?.status === 404) {
+    toast.error(
+      (result.error.data as { message?: string }).message || "Not Found"
+    );
+  }
+  if (result?.error?.status === 403) {
+    toast.error(
+      (result.error.data as { message?: string }).message || "Forbidden"
+    );
+  }
+
+  if (result?.error?.status === 401) {
+    //* Send Refresh
+    console.log("Sending refresh token");
+
+    const res = await fetch("http://localhost:5000/api/v1/auth/refresh-token", {
+      method: "POST",
+      credentials: "include",
+    });
+
+    const data = await res.json();
+
+    if (data?.data?.accessToken) {
+      const user = (api.getState() as RootState).auth.user;
+
+      api.dispatch(
+        setUser({
+          user,
+          token: data.data.accessToken,
+        })
+      );
+      result = await baseQuery(args, api, extraOptions);
+    } else {
+      api.dispatch(logout());
+    }
+
+    return result;
+  }
+};
+
 export const baseApi = createApi({
-  reducerPath: "",
-  baseQuery: baseQuery,
+  reducerPath: "baseApi",
+  baseQuery: baseQueryWithRefreshToken,
+  tagTypes: ["product"],
   endpoints: () => ({}),
 });

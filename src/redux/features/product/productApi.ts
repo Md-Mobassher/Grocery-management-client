f7a64d78b8b5394d60276c406d6279f5a25964cf
Md Mobassher Hossain <mobassherpautex@gmail.com>
2024-03-27 16:08:17 +0600

    product api added

---
+++ src/redux/features/product/productApi.ts
@@ -0,0 +1,76 @@
+import { baseApi } from "@/redux/api/baseApi";
+import { TQueryParam } from "../../../types/global.type";
+
+const productApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    getAllProducts: builder.query({
+      query: (args) => {
+        const params = new URLSearchParams();
+        if (args) {
+          args.forEach((item: TQueryParam) => {
+            if (item.value !== undefined) {
+              params.append(item.name, item.value as string);
+            }
+          });
+        }
+        return {
+          url: "/products",
+          method: "GET",
+          params: params,
+        };
+      },
+      providesTags: ["product"],
+    }),
+
+    getSingleProduct: builder.query({
+      query: (id) => ({
+        url: `/products/${id}`,
+        method: "GET",
+      }),
+    }),
+
+    addProduct: builder.mutation({
+      query: (newProduct) => ({
+        url: "/products",
+        method: "POST",
+        body: newProduct,
+      }),
+      invalidatesTags: ["product"],
+    }),
+
+    updateAProduct: builder.mutation({
+      query: ({ id, updatedProduct }) => ({
+        url: `products/${id}`,
+        method: "PATCH",
+        body: updatedProduct,
+      }),
+      invalidatesTags: ["product"],
+    }),
+
+    deleteAProduct: builder.mutation({
+      query: (id) => ({
+        url: `products/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["product"],
+    }),
+
+    bulkDeleteProducts: builder.mutation({
+      query: (productIds) => ({
+        url: `products/bulk-delete`,
+        method: "DELETE",
+        body: productIds,
+      }),
+      invalidatesTags: ["product"],
+    }),
+  }),
+});
+
+export const {
+  useGetAllProductsQuery,
+  useGetSingleProductQuery,
+  useAddProductMutation,
+  useUpdateAProductMutation,
+  useDeleteAProductMutation,
+  useBulkDeleteProductsMutation,
+} = productApi;

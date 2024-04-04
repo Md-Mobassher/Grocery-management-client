a35bc9deb2650429df9d8c6e13b60123134462c4
Md Mobassher Hossain <mobassherpautex@gmail.com>
2024-04-04 17:39:09 +0600

    banner design updated

--- src/components/common/Banner.tsx
+++ src/components/common/Banner.tsx
@@ -1,81 +1,136 @@
 import "swiper/css";
 import "swiper/css/bundle";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay, Pagination } from "swiper/modules";
+import { Autoplay, Navigation, Pagination } from "swiper/modules";
 
 import banner1 from "@/assets/banner/banner1.jpg";
 import banner2 from "@/assets/banner/banner1.jpg";
 import banner3 from "@/assets/banner/banner3.jpg";
-import bannergif from "@/assets/banner/sidebar-banner.gif";
+import { Button } from "../ui/button";
 
 const Banner = () => {
   return (
-    <div className="max-w-7xl mx-auto">
-      <div className="flex justify-between items-stretch gap-5 max-h-[550px] lg:px-3 md:px-5 px-4">
-        <div className="lg:max-w-3/4 md:max-w-2/3 w-full">
-          <Swiper
-            spaceBetween={0}
-            slidesPerView={1}
-            autoplay={{
-              delay: 2500,
+    <div className="max-w-7xl mx-auto lg:h-[550px] md:h-[450px] h-[300px]">
+      <Swiper
+        spaceBetween={0}
+        slidesPerView={1}
+        autoplay={{
+          delay: 2500,
+        }}
+        loop={true}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="w-full h-full rounded-lg"
+      >
+        <SwiperSlide className="flex visible h-full">
+          <div
+            style={{
+              backgroundImage: `url(${banner1})`,
+              backgroundRepeat: "no-repeat",
+              backgroundSize: "cover",
             }}
-            loop={true}
-            pagination={{
-              clickable: true,
-            }}
-            navigation={true}
-            modules={[Autoplay, Pagination]}
-            className="max-w-4xl h-full rounded-lg"
+            className="border flex justify-start items-center w-full h-full"
           >
-            <SwiperSlide className="flex visible">
-              <div
-                style={{
-                  display: "flex",
-                  justifyContent: "space-between",
-                  backgroundImage: `url(${banner1})`,
-                  height: "550px",
-                }}
-                className="border"
-              >
-                <p>slide1</p>
+            <div className="lg:px-20 md:px-10 px-6 lg:max-w-xl max-w-xs">
+              <div className="flex justify-start items-center uppercase font-semibold pb-2 ">
+                <p className="lg:text-lg text-md">Exclusive Offer</p>
+                <p className="bg-green-200 px-3 py-2 ml-2 rounded-xl bg-gradient-to-r lg:text-lg text-md">
+                  -20% OFF
+                </p>
               </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div
-                style={{
-                  display: "flex",
-                  justifyContent: "space-between",
-                  backgroundImage: `url(${banner2})`,
-                  height: "550px",
-                }}
-                className="border"
-              >
-                <p>slide2</p>
+              <h2 className="lg:text-4xl md:text-3xl text-2xl font-bold lg:pb-5 pb-3">
+                Specialist in the grocery store
+              </h2>
+              <p>Only this week. Don't miss...</p>
+              <div className="flex justify-start items-center mt-2 lg:mb-5 mb-3">
+                <p>from </p>
+                <p className="text-3xl font-semibold text-green-400 ml-2">
+                  $7.99
+                </p>
               </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div
-                style={{
-                  display: "flex",
-                  justifyContent: "space-between",
-                  backgroundImage: `url(${banner3})`,
-                  height: "550px",
-                }}
-                className="border"
+              <Button
+                className="bg-green-400 hover:bg-green-500 transition-all duration-300 rounded-3xl px-6 "
+                type="submit"
               >
-                <p>slide3</p>
+                Shop Now
+              </Button>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className="flex visible h-full">
+          <div
+            style={{
+              backgroundImage: `url(${banner2})`,
+              backgroundRepeat: "no-repeat",
+              backgroundSize: "cover",
+            }}
+            className="border flex justify-start items-center w-full h-full"
+          >
+            <div className="lg:px-20 md:px-10 px-6 lg:max-w-xl max-w-xs">
+              <div className="flex justify-start items-center uppercase font-semibold pb-2 ">
+                <p className="lg:text-lg text-md">Exclusive Offer</p>
+                <p className="bg-green-200 px-3 py-2 ml-2 rounded-xl bg-gradient-to-r lg:text-lg text-md">
+                  -20% OFF
+                </p>
               </div>
-            </SwiperSlide>
-          </Swiper>
-        </div>
-        <div className=" lg:max-w-1/4 md:max-w-1/3 lg:block md:block hidden">
-          <img
-            className="rounded-lg border lg:max-w-96 "
-            src={bannergif}
-            alt=""
-          />
-        </div>
-      </div>
+              <h2 className="lg:text-4xl md:text-3xl text-2xl font-bold lg:pb-5 pb-3">
+                Feed your family the best
+              </h2>
+              <p>Only this week. Don't miss...</p>
+              <div className="flex justify-start items-center mt-2 lg:mb-5 mb-3">
+                <p>from </p>
+                <p className="text-3xl font-semibold text-green-400 ml-2">
+                  $8.99
+                </p>
+              </div>
+              <Button
+                className="bg-green-400 hover:bg-green-500 transition-all duration-300 rounded-3xl px-6 "
+                type="submit"
+              >
+                Shop Now
+              </Button>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className="flex visible h-full">
+          <div
+            style={{
+              backgroundImage: `url(${banner3})`,
+              backgroundRepeat: "no-repeat",
+              backgroundSize: "cover",
+            }}
+            className="border flex justify-start items-center w-full h-full"
+          >
+            <div className="lg:px-20 md:px-10 px-6 lg:max-w-xl max-w-xs">
+              <div className="flex justify-start items-center uppercase font-semibold pb-2 ">
+                <p className="lg:text-lg text-md">Exclusive Offer</p>
+                <p className="bg-green-200 px-3 py-2 ml-2 rounded-xl bg-gradient-to-r lg:text-lg text-md">
+                  -20% OFF
+                </p>
+              </div>
+              <h2 className="lg:text-4xl md:text-3xl text-2xl font-bold lg:pb-5 pb-3">
+                Grocery full of inspiration
+              </h2>
+              <p>Only this week. Don't miss...</p>
+              <div className="flex justify-start items-center mt-2 lg:mb-5 mb-3">
+                <p>from </p>
+                <p className="text-3xl font-semibold text-green-400 ml-2">
+                  $6.99
+                </p>
+              </div>
+              <Button
+                className="bg-green-400 hover:bg-green-500 transition-all duration-300 rounded-3xl px-6 "
+                type="submit"
+              >
+                Shop Now
+              </Button>
+            </div>
+          </div>
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 };
